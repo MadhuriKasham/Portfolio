5ecc1b942db2d41d1462b8ba838f5d0dab04f7d2
Madhuri Kasham <70937030+MadhuriKasham@users.noreply.github.com>
2023-09-27 22:09:31 +0530

    Add files via upload

---
+++ Portfolio/assets/js/script.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed('#element', {
+    strings: ['Web Scraper', 'Python Developer', 'Data Engineer'],
+    typeSpeed: 50,
+  });
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark");
+  // if(element.classList.toggle("dark")){
+  //   element.src = "sun.png";
+  // }else{
+  //   element.src = "moon.png";
+  // }
+}
+
+
+
+
+
+
